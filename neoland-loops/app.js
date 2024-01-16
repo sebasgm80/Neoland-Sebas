c9d8d5e2f26a856655c58bd971183cbff19680e1
Sebastian Garcia Muñoz <sebas@AirdeSebastian.home>
2024-01-16 17:57:28 +0100

    martes 1,1

---
+++ neoland-loops/app.js
@@ -0,0 +1,14 @@
+// Iteración #1: Buscar el máximo
+
+function sum(numberOne , numberTwo) {
+    // insert code
+}
+
+// Iteración #2: Buscar la palabra más larga
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(param) {
+  // insert code
+}
+
+// Iteración #3: Calcular la suma
